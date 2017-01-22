3295c78faa3273f1eef084928b82e93b0c16811d
nzoLogic <nzoLogic@gmail.com>
2017-01-21 17:00:37 -0800

    edit html, controller and model operational

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,24 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
-  def index
-    @articles = Article.all
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -16,7 +30,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
